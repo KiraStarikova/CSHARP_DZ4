5aac5edad4dede44b8198c61febb4c3b634fa2a3
Kira <starkial@lenta.ru>
2022-08-18 11:37:28 +0500

    Task 29 создание и вывод массива на экран

---
+++ Task_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// задаёт массив из 8 элементов и выводит их на экран
+
+int [] ArrayEight(int len)
+{
+    int[] arr = new int [len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(0,9);
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+PrintArray(ArrayEight(8));
