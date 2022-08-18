ad225c7a60264aeb45da89a6cd57bd6fee1472ce
Kira <starkial@lenta.ru>
2022-08-18 11:30:30 +0500

    Task 025 формула возведения в степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,9 @@
+﻿// принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+void Power(int a, int b)
+{
+    int result =Convert.ToInt32(Math.Pow(a, b));
+    Console.WriteLine($"Число {a} в степени {b} равняется {result}");
+}
+Power(5, 8);
